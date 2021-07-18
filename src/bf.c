81f2dfaafbfc0c0e2cf4c5c1ce7be2475cd26924
acke756 <acke756@gmail.com>
2021-07-18 22:21:27 +0200

    Added struct bf_context and extracted function interpret()

--- src/bf.c
+++ src/bf.c
@@ -1,42 +1,39 @@
 #include "bf.h"
 
-int bf_interpret_file(FILE *f, const size_t mem_siz, char *const mem) {
-    if (mem == NULL) {
-        char new_mem[mem_siz];
-        return bf_interpret_file(f, mem_siz, new_mem);
-    }
+typedef struct bf_context {
+    FILE *f;
+    const size_t mem_siz;
+    char *const mem;
+    char *ptr;
+} bf_context_t;
 
-    char *ptr = mem;
+int interpret(bf_context_t *context_p) {
     int in;
 
-    for (size_t i = 0; i < mem_siz; i++) {
-        mem[i] = 0;
-    }
-
-    for (int c = fgetc(f); c != EOF; c = fgetc(f)) {
+    for (int c = fgetc(context_p->f); c != EOF; c = fgetc(context_p->f)) {
         switch (c) {
             case '>':
-                ptr++;
-                if (ptr >= mem + mem_siz) {
+                (context_p->ptr)++;
+                if (context_p->ptr >= context_p->mem + context_p->mem_siz) {
                     return -1;
                 }
 
                 break;
             case '<':
-                ptr--;
-                if (ptr < mem) {
+                (context_p->ptr)--;
+                if (context_p->ptr < context_p->mem) {
                     return -1;
                 }
 
                 break;
             case '+':
-                (*ptr)++;
+                (*(context_p->ptr))++;
                 break;
             case '-':
-                (*ptr)--;
+                (*(context_p->ptr))--;
                 break;
             case '.':
-                if (putchar(*ptr) == EOF) {
+                if (putchar(*(context_p->ptr)) == EOF) {
                     return -1;
                 }
 
@@ -47,12 +44,34 @@ int bf_interpret_file(FILE *f, const size_t mem_siz, char *const mem) {
                     return -1;
                 }
 
-                *ptr = (char) in;
+                *(context_p->ptr) = (char) in;
                 break;
         }
     }
 
     return 0;
+}
+
+int bf_interpret_file(FILE *f, const size_t mem_siz, char *const mem) {
+    if (mem == NULL) {
+        char new_mem[mem_siz];
+        return bf_interpret_file(f, mem_siz, new_mem);
+    }
+
+    for (size_t i = 0; i < mem_siz; i++) {
+        mem[i] = 0;
+    }
+
+    char *ptr = mem;
+
+    bf_context_t context = {
+        .f = f,
+        .mem_siz = mem_siz,
+        .mem = mem,
+        .ptr = ptr,
+    };
+
+    return interpret(&context);
 }
 
 int bf_interpret(char *path, const size_t mem_siz, char *const mem) {
