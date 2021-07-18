c25dd610a88f61c308b98f03304225f5ccd47ce7
acke756 <acke756@gmail.com>
2021-07-18 16:26:58 +0200

    Added unit test for + command.

--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "bf.h"
 
@@ -20,11 +21,21 @@ bool test_leave_right(void) {
     return bf_interpret("bf/four_right.bf", 4, NULL) != 0;
 }
 
+bool test_four_rising(void) {
+    char mem_ref[] = {0, 1, 2, 3};
+    char mem[4] = {0};
+
+    bf_interpret("bf/four_rising.bf", 4, mem);
+
+    return memcmp(mem, mem_ref, 4UL) == 0;
+}
+
 int main(int argc, char **argv) {
     test_t testv[] = {
         {test_sanity, "Sanity check"},
         {test_leave_left, "Error when pointer exits left"},
         {test_leave_right, "Error when pointer exits right"},
+        {test_four_rising, "Four rising numbers"},
     };
     size_t testc = sizeof(testv) / sizeof(test_t);
 
